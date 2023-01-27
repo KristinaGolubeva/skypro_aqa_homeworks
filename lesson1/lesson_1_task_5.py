28989e2f4ca19686b348fc5d6d1fae9ab11755fe
KristinaGolubeva <kristina_g26@yahoo.com>
2023-01-27 18:44:55 +0300

    Создала файлы домашки

---
+++ lesson1/lesson_1_task_5.py
@@ -0,0 +1,6 @@
+my_age = input()
+print("Ваш возраст: ", my_age)
+
+my_age = int(my_age) + 1
+
+print(my_age)
---
+++ lesson1/lesson_1_task_8.py
@@ -0,0 +1,14 @@
+def myfunc(num):
+    print(num, end='')
+
+myfunc(8)
+myfunc(8)
+myfunc(0)
+myfunc(0)
+myfunc(5)
+myfunc(5)
+myfunc(5)
+myfunc(3)
+myfunc(5)
+myfunc(3)
+myfunc(5)
