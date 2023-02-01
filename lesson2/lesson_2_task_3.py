32d31878b11e407176532b863f38344015ce906c
KristinaGolubeva <kristina_g26@yahoo.com>
2023-02-01 11:49:20 +0300

    Исправила ошибки

--- lesson2/lesson_2_task_3.py
+++ lesson2/lesson_2_task_3.py
@@ -1,5 +1,12 @@
+#def square(a):
+#    print(round(a * a))
+
+#square(1.6)
+
+import math
+
 def square(a):
-    print(round(a * a))
+    print(math.ceil(a * a))
 
 square(1.6)
 
--- lesson2/lesson_2_task_4.py
+++ lesson2/lesson_2_task_4.py
@@ -1,7 +1,7 @@
 
 def fizz_buzz(n):
-    for i in range(1,n):
-        if (i % 3 == 0) and (n % 5 == 0):
+    for i in range(1,n+1):
+        if (i % 3 == 0) and (i % 5 == 0):
             print('FizzBuzz')
         elif (i % 3 == 0):
             print("Fizz")
